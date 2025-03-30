fa9a4acdf95f28d7a285e57d2d9c38e88d8e7838
CamilaDB <cbazzo@hotmail.com>
2025-03-30 09:59:39 -0300

    add blog details

---
+++ src/BlogDetails.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  return (
+    <div className="blog-details">
+      <h2>Blog Details</h2>
+      <p>{id}</p>
+    </div>
+  );
+};
+
+export default BlogDetails;
